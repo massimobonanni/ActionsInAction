d68069726bf4656e793d6ebc40c6173d75782391
Massimo Bonanni <massimo.bonanni@tiscali.it>
2021-03-06 09:44:51 +0100

    First commit

---
+++ MyAmazingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyAmazingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
